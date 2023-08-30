9ad88f6e4359f1051b4b420cae4450b96d079cdb
jarriola-sn <jaime@simplenight.com>
2023-08-30 19:53:31 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { NextPageContext } from "next"
+import { useSession, signIn, signOut, getSession } from "next-auth/react"
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const {data:session} = useSession()
+  console.log(session)
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+    >
+     <h6>{session?.user?.name}</h6>
+     <img src={session?.user?.image!} alt="" className="w-32 h-32 rounded-full" />
+     {
+      session ? <button onClick={() => signOut()}>Sign out</button> : <button onClick={() => signIn()}>Sign in</button>
+     }
+    </main>
+  )
+}
+
+export async function getServerSideProps(ctx:NextPageContext) {
+  const session = await getSession(ctx);
+  return {
+    props:{
+      session
+    }
+  }
+  
+}
