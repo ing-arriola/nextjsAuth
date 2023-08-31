2c48a9f7a61f51860a9ea4732586951296efbb51
jarriola-sn <jaime@simplenight.com>
2023-08-30 20:07:48 -0400

    removed console

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const {data:session} = useSession()
-  console.log(session)
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
