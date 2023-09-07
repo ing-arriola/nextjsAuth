87bc64e5610998f2e9c4d85f4ab3de20740f7fc2
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-07 14:49:45 -0400

    Added ToastContainer to show notifications everywhere

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import '@/styles/globals.css'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps: {session, ...pageProps}}: AppProps) {
   return (
     <SessionProvider session={session}>
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Component {...pageProps} />
     </SessionProvider>
   )
