065b98df67cdfba0624803bdbf5fa4419efb0814
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-07 00:12:45 -0400

    Added component to show how strong a password is

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { FieldError, FieldErrorsImpl, FieldValues, Merge, UseFormRegister } from 'react-hook-form';
 import {IoAlertCircle} from 'react-icons/io5'
+import {ImEye,ImEyeBlocked} from 'react-icons/im'
 
 interface IInputProps {
     name:string;
@@ -15,7 +17,7 @@ interface IInputProps {
 
 const Input: React.FunctionComponent<IInputProps> = (props) => {
     const {name,label,type,icon,placeholder, register, error, disabled} = props
-    
+    const [showPassword, setshowPassword] = useState(false)
   return (
   <section className="mt-3 w-[100%]">
     <label htmlFor={name} className="text-gray-700">
@@ -30,7 +32,7 @@ const Input: React.FunctionComponent<IInputProps> = (props) => {
             </span>
         </div>
         <input 
-            type={type}
+            type={showPassword ? 'text' : type}
             className='w-full py-2 pr-7 pl-8 block rounded-md border-gray-300 outline-offset-1 outline-transparent focus:border-blue-500 focus:ring-blue-700 focus:ring-1 text-sm'
             placeholder={placeholder}
             style={{
@@ -38,6 +40,15 @@ const Input: React.FunctionComponent<IInputProps> = (props) => {
             }}
             {...register(name)}
         />
+        {
+            (name === 'password' || name === "confirmPassword") &&
+                <div className="absolute top-2.5 right-2 text-xl text-gray-700 cursor-pointer"
+                     style={{right:`${error?'2rem':''}`}}
+                     onClick={()=>setshowPassword(prev=>!prev)}
+                >
+                    {showPassword ? <ImEye/> : <ImEyeBlocked/>}
+                </div>
+        }
         {error && (
                 <div className="fill-red-500 absolute right-2 top-2.5 text-xl">
                 <IoAlertCircle fill='#ED4337' />
