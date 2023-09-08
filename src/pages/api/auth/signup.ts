c14160b2b3bd7902c411c08fe707c8af18fbb400
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-08 19:18:24 -0400

    added interaction to send email

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,10 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import User from '@/models/User';
+import User from "@/models/User";
 import connectDB from '@/utils/connectDB'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import validator from 'validator';
-import bcrypt from 'bcryptjs';
+import type { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import { createActivationToken } from "@/utils/tokens";
 
+import { activateTemplateEmail } from "@/emailTemplates/activate";
+import sendMail from "@/utils/sendMails";
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,39 +14,57 @@ export default async function handler(
 ) {
   try {
     await connectDB();
-    const { first_name, last_name, email, phone, password }= req.body;
-    if(!first_name || !last_name || !email || !phone || !password){
-        return res.status(400).json({message:'Please fill in all the fields'});
+    const { first_name, last_name, email, phone, password } = req.body;
+    if (!first_name || !last_name || !email || !phone || !password) {
+      return res.status(400).json({ message: "Please fill in all fields." });
     }
-    if(!validator.isEmail(email)){
-        return res.status(400).json({message:'Please add a valid email'});
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ message: "Please Add a valid email address." });
     }
-    if(!validator.isMobilePhone(phone)){
-        return res.status(400).json({message:'Please add a valid phone number'});
+    if (!validator.isMobilePhone(phone)) {
+      return res
+        .status(400)
+        .json({ message: "Please Add a valid phone number." });
     }
-    const user = await User.findOne({email:email})
-        if(user){
-            return res
-                    .status(400)
-                    .json({message:"This email already exists"})
-        }
-        if(password.length < 6){
-            return res
-                    .status(400)
-                    .json({message:"Password must be at least 6 characters"})
-        }
-        const cryptedPassword = await bcrypt.hash(password,12); 
-        const newUser = new User({
-            name:`${first_name} ${last_name}`,
-            email,
-            phone,
-            password: cryptedPassword
-        })
-        await newUser.save();
-        res.json({message:"User registered successfully!, Please activate your account"})
-    
+    const user = await User.findOne({
+      email: email,
+    });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "This email address already exists." });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters." });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newuser = new User({
+      name: `${first_name + " " + last_name}`,
+      email,
+      phone,
+      password: cryptedPassword,
+    });
+    await newuser.save();
+    const activation_token = createActivationToken({
+      id: newuser._id.toString(),
+    });
+    const url = `${process.env.NEXTAUTH_URL}/activate/${activation_token}`;
+    await sendMail(
+      newuser.email,
+      newuser.name,
+      "",
+      url,
+      "Activate your account",
+      activateTemplateEmail
+    );
+    res.json({
+      message: "Register success! Please activate your account to start.",
+    });
   } catch (error) {
-    res.status(500).json({message:(error as Error).message})
+    res.status(500).json({ message: (error as Error).message });
   }
-} 
-
+}
