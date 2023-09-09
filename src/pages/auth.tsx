5c2c4593911b4f45b051c2246b9a08d75a1f81ad
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-09 19:51:04 -0400

    sending callback and csfr token to login form

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -2,15 +2,16 @@ import Background from "@/components/backgrounds/Background";
 import LoginForm from "@/components/forms/Login";
 import RegisterForm from "@/components/forms/Register";
 import { NextPageContext } from "next";
+import { getCsrfToken } from "next-auth/react";
 
-export default function Auth({tab}:{tab:string}) {
+export default function Auth({tab, callbackUrl, csfrToken}:{tab:string,callbackUrl:string, csfrToken:string}) {
   return (
     <div className="w-full flex items-center justify-center">
         <div className="w-full h-100 flex items-center justify-center">
             {/* FORM */}
             <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white">
                     {
-                        tab === 'signin' ? <LoginForm/> : <RegisterForm/>
+                        tab === 'signin' ? <LoginForm callbackUrl = {callbackUrl} csfrToken={csfrToken} /> : <RegisterForm/>
                     }
                     
             </div>
@@ -24,9 +25,13 @@ export default function Auth({tab}:{tab:string}) {
 export const getServerSideProps = async (ctx:NextPageContext) => {
     const { req,query } = ctx
     const tab = query.tab ? query.tab : "signin"
+    const callbackUrl = query.callbackUrl ? query.callbackUrl  : process.env.NEXTAUTH_URL 
+    const csfrToken = await getCsrfToken(ctx)
     return {
         props:{
-            tab:JSON.parse(JSON.stringify(tab))
+            tab:JSON.parse(JSON.stringify(tab)),
+            callbackUrl,
+            csfrToken
         }
     }
 }
