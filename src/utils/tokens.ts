b9fa991118a238588d274e68efa577c46db03d8a
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-09 17:29:56 -0400

    updated actvation and reset functions

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -2,12 +2,9 @@ import jwt from 'jsonwebtoken'
 const { ACTIVATION_TOKEN_SECRET, RESET_TOKEN_SECRET } = process.env
 
 export const createActivationToken = (payload:any) => {
-    return jwt.sign(payload,ACTIVATION_TOKEN_SECRET!),{
-        expiresIn:'2d'
-    }
+    console.log('jwt.sign(payload,ACTIVATION_TOKEN_SECRET!)',jwt.sign(payload,ACTIVATION_TOKEN_SECRET!))
+    return jwt.sign(payload,ACTIVATION_TOKEN_SECRET!)
 }
 export const createResetToken = (payload:any) => {
-    return jwt.sign(payload,RESET_TOKEN_SECRET!),{
-        expiresIn:'6H'
-    }
+    return jwt.sign(payload,RESET_TOKEN_SECRET!)
 }
