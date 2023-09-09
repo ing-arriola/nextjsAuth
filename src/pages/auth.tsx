5913fef3b1578623194ebe3bb51e7004dd8240b5
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-09 18:24:09 -0400

    getting props to check what's the current tab

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,28 +1,32 @@
 import Background from "@/components/backgrounds/Background";
+import LoginForm from "@/components/forms/Login";
 import RegisterForm from "@/components/forms/Register";
+import { NextPageContext } from "next";
 
-export default function Auth() {
+export default function Auth({tab}:{tab:string}) {
   return (
     <div className="w-full flex items-center justify-center">
         <div className="w-full h-100 flex items-center justify-center">
             {/* FORM */}
             <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white">
-                <div className="w-full px-12 py-4">
-                    <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">
-                        Sign up
-                    </h2>
-                    <p className="text-center text-sm text-gray-600 mt-2">
-                        Do you already have an account? &nbsp;
-                        <a href="" className="text-blue-600 hover:text-blue-700 hover:underline cursor-pointer">
-                            Sign in
-                        </a>
-                    </p>
-                    <RegisterForm/>
-                </div>
+                    {
+                        tab === 'signin' ? <LoginForm/> : <RegisterForm/>
+                    }
+                    
             </div>
             {/* BACKGROUND */}
-            <Background image="../../auth/register.jpg"/> 
+            <Background image={`"../../auth/${tab === "signup" ? "register" : "login"}.jpg"`}/> 
         </div>
     </div>
   )
+}
+
+export const getServerSideProps = async (ctx:NextPageContext) => {
+    const { req,query } = ctx
+    const tab = query.tab ? query.tab : "signin"
+    return {
+        props:{
+            tab:JSON.parse(JSON.stringify(tab))
+        }
+    }
 }
